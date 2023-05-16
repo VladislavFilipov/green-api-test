8cdf237f58d809479034867c11d08a7d5a6dd05b
Vladislav Filipov <vlad1philip@gmail.com>
2023-05-16 17:45:05 +0500

    fix eslint error

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -1,8 +1,8 @@
-// styled.d.ts
 import "styled-components";
 
 import { ITheme } from "@src/types/theme.types";
 
 declare module "styled-components" {
+  /* eslint-disable */
   export interface DefaultTheme extends ITheme {}
 }
